f55dfaa18eb12de2c6c56adeb99ccb6ca94bb3f2
Steven Mathew <mathewsteven92@gmail.com>
2026-03-31 01:19:11 -0400

    New changes push

--- supabase/migrations/20241225_fix_history_rls.sql
+++ supabase/migrations/20241225_fix_history_rls.sql
@@ -1,5 +1,5 @@
 -- Add INSERT policy for site_history
--- This allows authenticated users to add history records for sites they own
+-- This allows authenticated users to add history record for sites they own
 
 create policy "Users can insert history of own sites"
   on public.site_history for insert
